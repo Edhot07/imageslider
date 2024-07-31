5c124c4c8f7935dd4bf2f7cb211995937f5c8152
Edhot07 <158280720+Edhot07@users.noreply.github.com>
2024-07-31 11:34:22 +0530

    added Navbar.jsx

---
+++ Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='ul'>
+        <h1>Gallery</h1>
+        <h2>Countries Map</h2>
+    </div>
+  );
+}
+
+export default Navbar;
